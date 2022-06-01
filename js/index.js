f7a19ef9cc6c1a061c020db7db2954b5563f0ea3
werner94fribourg <werner94@hotmail.com>
2022-06-01 17:35:07 +0200

    Main Block 2 Card decomposition

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
+'use strict';
+
+// Variables
+const cardElements = document.querySelectorAll(
+  '.block.block--1 .block__content .content__item .item__img, .activity__content .content__item .item__img, .activity__content .content__item.expand .item__img, .activity__content .content__item.reduce .item__img'
+);
+
+const activityCardElements = document.querySelectorAll(
+  '.activity__content .content__item'
+);
+
+// Functions
 const changeCardHeight = () => {
-  var cardElements = document.querySelectorAll(
-    '.block.block--1 .block__content .content__item .item__img'
-  );
   cardElements.forEach(card => {
     if (document.documentElement.clientWidth <= 768) {
       const newHeight = (card.clientWidth / 768) * 280;
@@ -12,5 +21,32 @@ const changeCardHeight = () => {
   });
 };
 
-changeCardHeight();
+const changeActivityCardHeight = () => {
+  activityCardElements.forEach(card => {
+    if (document.documentElement.clientWidth > 992) {
+      const cardImg = card.querySelector('.item__img');
+      let newHeight;
+      switch (true) {
+        case card.classList.contains('expand'):
+          newHeight = (cardImg.clientHeight / 500) * 400;
+          break;
+        case card.classList.contains('reduce'):
+          newHeight = (cardImg.clientHeight / 265) * 200;
+          break;
+        default:
+          console.log('Default');
+          newHeight = (cardImg.clientHeight / 215) * 150;
+          break;
+      }
+      cardImg.style.height = newHeight.toFixed(2) + 'px';
+    } else {
+      card.removeAttribute('style');
+    }
+  });
+};
+
+// Event Listeners
 window.addEventListener('resize', changeCardHeight);
+window.addEventListener('load', changeCardHeight);
+/*window.addEventListener('resize', changeActivityCardHeight);
+window.addEventListener('load', changeActivityCardHeight);*/
